3f194a8982d6632f74e4f4055751c91fe10557b6
rlabbiz <redalabbiz@gmail.com>
2024-12-07 11:59:22 +0100

    change code

--- config/connect.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-export default async function connect() {
-
-  await mongoose.connect('mongodb://localhost:27017/test')
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error(err));
-}
